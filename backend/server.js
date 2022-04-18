5669d002da66276d6c26e1f728b92c3003ef1966
WebDev Journey <angeliesioson@gmail.com>
2022-04-18 16:40:12 +0800

    authenticate part

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,26 @@
 console.log("server...")
+const express = require("express")
+const colors = require("colors")
+const dotenv = require("dotenv").config()
+const { errorHandler } = require("./middleware/errorMiddleware")
+const connectDB = require("./config/db")
+const PORT = process.env.PORT || 8000
+
+// Connect to database
+connectDB()
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.get("/", (req, res) => {
+  res.send("Hello")
+})
+
+// Routes
+app.use("/api/users", require("./routes/userRoutes"))
+
+app.use(errorHandler)
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
---
+++ frontend/src/App.js
@@ -0,0 +1,30 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { ToastContainer } from "react-toastify"
+//import Toast from "react-toastify/dist/components"
+import "react-toastify/dist/ReactToastify.css"
+import Header from "./components/Header"
+
+//Pages
+import Home from "./pages/Home"
+import Login from "./pages/Login"
+import Register from "./pages/Register"
+
+function App() {
+  return (
+    <>
+      <Router>
+        <div className="container">
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </div>
+      </Router>
+      <ToastContainer />
+    </>
+  )
+}
+
+export default App
